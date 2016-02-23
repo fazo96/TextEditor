4e826a93555b2631e404e606184d502da37358bf
Enrico Fasoli <fazius2009@gmail.com>
2016-02-23 21:31:52 +0100

    implemented saving and loading to files

--- src/net/OperationConverter.java
+++ src/net/OperationConverter.java
@@ -5,6 +5,15 @@
  */
 package net;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import operations.AddOperation;
 import operations.DelOperation;
 import operations.NullOperation;
@@ -69,6 +78,28 @@ public class OperationConverter {
                 return new NullOperation(base);
         }
         System.err.println("Unknown Operation \"" + ss[2] + "\"");
+        return null;
+    }
+
+    public void save(Operation o, File f) {
+        try {
+            PrintWriter writer = new PrintWriter(f, "UTF-8");
+            writer.write(o.evaluate());
+            writer.close();
+        } catch (Exception ex) {
+            Logger.getLogger(OperationConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String load(File f) {
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+            StringBuilder sb = new StringBuilder();
+            bf.lines().forEach((String s) -> sb.append(s).append('\n'));
+            return sb.toString();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(OperationConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 }
